14abd7785b091a27ccf54218a9cacc2313e90357
Gruskas <gruskaspl@gmail.com>
2024-11-29 10:31:31 +0100

    MORE Arguments

--- src/main/java/org/gruskas/Arguments.java
+++ src/main/java/org/gruskas/Arguments.java
@@ -3,11 +3,70 @@ package org.gruskas;
 public class Arguments {
 
     public static void checkArguments(String[] args) {
-        for (String arg : args) {
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
             switch (arg) {
                 case "-h":
                 case "--help":
                     printHelp();
+                    return;
+                case "-f":
+                case "-o":
+                case "--file":
+                case "--open":
+                    if (i + 1 < args.length) {
+//                        System.out.println(args[i + 1]);
+                        i++;
+                        ArgumentsEditor.ReadFile(args[i]);
+
+                    } else {
+                        TerminalUI.Error("Missing file name after " + arg);
+                    }
+                    break;
+                case "-d":
+                case "--delate":
+                    if (i + 1 < args.length) {
+//                        System.out.println(args[i + 1]);
+                        i++;
+                        ArgumentsEditor.DeleteFile(args[i]);
+
+                    } else {
+                        TerminalUI.Error("Missing file name after " + arg);
+                    }
+                    break;
+                case "-ul":
+                case "--update-line":
+                    if (i + 1 < args.length) {
+//                        System.out.println(args[i + 1]);
+                        i++;
+                        String path = args[i];
+                        i++;
+                        int line = Integer.parseInt(args[i]);
+                        System.out.println(line);
+                        i++;
+                        String newContent = args[i];
+                        i++;
+                        ArgumentsEditor.UpdateLine(path, line, newContent);
+                    } else {
+                        TerminalUI.Error("Missing file name after " + arg);
+                    }
+                    break;
+                case "-a":
+                case "--append":
+                    if (i + 1 < args.length) {
+                        i++;
+                        String path = args[i];
+                        i++;
+                        String newContent = args[i];
+                        i++;
+                        ArgumentsEditor.WriteToFile(path, newContent, true);
+                    } else {
+                        TerminalUI.Error("Missing file name after " + arg);
+                    }
+                    break;
+                case "-b":
+                case "--banner":
+                    TerminalUI.printBanner();
                     return;
                 default:
                     TerminalUI.Error("Unknown argument: " + arg);
@@ -19,6 +78,13 @@ public class Arguments {
     private static void printHelp() {
         System.out.println("Usage: java -jar JavaViNote-1.0-SNAPSHOT.jar [options]");
         System.out.println("Options:");
-        System.out.println("  -h, --help       Show this help message.");
+        System.out.println("  -h, --help                 Show this help message.");
+        System.out.println("  -f, --file <filename>      Open the specified file.");
+        System.out.println("  -o, --open <filename>      Open the specified file (alias for -f).");
+        System.out.println("  -d, --delete <filename>    Delete the specified file.");
+        System.out.println("  -ul, --update-line <file> <line> <newContent>");
+        System.out.println("                             Update a specific line in the file with new content.");
+        System.out.println("  -a, --append <file> <content>");
+        System.out.println("                             Append content to the specified file.");
     }
 }
---
+++ src/main/java/org/gruskas/ArgumentsEditor.java
@@ -0,0 +1,69 @@
+package org.gruskas;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArgumentsEditor {
+    public static void ReadFile(String path) {
+        try {
+            int i = 1;
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(i + ". " + data);
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            TerminalUI.Error("An error occurred while reading the file: ", e.getMessage());
+        }
+    }
+
+    public static void DeleteFile(String path) {
+        File file = new File(path);
+//        System.out.println(file.getAbsolutePath());
+        if (file.delete()) {
+            TerminalUI.success("The file was successfully deleted: ", file.getName());
+        } else {
+            TerminalUI.Error("This file does not exist");
+        }
+    }
+
+
+    public static void UpdateLine(String path, int lineNumber, String newContent) {
+        Path filePath = Paths.get(path);
+        try {
+            ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(filePath);
+
+            if (lineNumber > 0 && lineNumber <= lines.size()) {
+                String formattedDate = FileOperations.DateTimeNow();
+                lines.set(lineNumber - 1, newContent + " | " + formattedDate);
+                Files.write(filePath, lines);
+               System.out.println("Line " + lineNumber + " has been updated in the file: " + path);
+            } else {
+                TerminalUI.Error("Invalid line number.");
+            }
+        } catch (IOException e) {
+            TerminalUI.Error("An error occurred while updating the line: ", e.getMessage());
+        }
+    }
+
+    public static void WriteToFile(String path, String content, Boolean append) {
+        try {
+            String formattedDate = FileOperations.DateTimeNow();
+            FileWriter Writer = new FileWriter(path, append);
+
+            Writer.write(content + " | " + formattedDate + System.lineSeparator());
+            Writer.close();
+        } catch (IOException e) {
+            TerminalUI.Error("An error occurred while writing to the file: ", e.getMessage());
+        }
+    }
+}
